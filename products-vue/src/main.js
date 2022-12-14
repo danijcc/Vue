af0d91793a3f3800076f598b800c56211ca06da6
danijcc <danicolmenares7@gmail.com>
2022-12-14 13:44:59 -0500

    import axios

--- products-vue/src/main.js
+++ products-vue/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css';
-import '@fontawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createApp(App).use(store).use(router).mount('#app')
 
--- products-vue/src/views/HomeView.vue
+++ products-vue/src/views/HomeView.vue
@@ -1,4 +1,36 @@
 <template>
+<div class="row">
+    <div class="col-lg-8 offset-lg-2">
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+                <thead><tr><th>#</th><th>NOMBRE</th><th>COMIDA</th><th>FECHA</th></tr></thead>
+                <tbody class="table-group-divider" id="contenido">
 
+                </tbody>
+            </table>
+        </div> 
+    </div>
+</div>
 </template>
+<script>
+    import axios from "axios";
+    export default{
+        data(){
+            return {foods:null}
+        },
+        mounted(){
+            this.getFoods();
+        },
+        methods:{
+            getFoods(){
+                axios.get('http://127.0.0.1:8000/api/foods').then(
+                    response =>(
+                        this.products = response.data
+                    )
+                );
+            }
+        }
+
+    }
+</script>
 
