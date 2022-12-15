7653778d8755650f5d85c661d47aeb667125af2c
danijcc <danicolmenares7@gmail.com>
2022-12-15 12:23:51 -0500

    axios and v-for

--- products-vue/src/views/HomeView.vue
+++ products-vue/src/views/HomeView.vue
@@ -3,9 +3,19 @@
     <div class="col-lg-8 offset-lg-2">
         <div class="table-responsive">
             <table class="table table-bordered table-hover">
-                <thead><tr><th>#</th><th>NOMBRE</th><th>COMIDA</th><th>FECHA</th></tr></thead>
+                <thead><tr><th>#</th><th>NOMBRE</th><th>FECHA</th><th>FECHA2</th><th>ACCIONES</th></tr></thead>
                 <tbody class="table-group-divider" id="contenido">
+                    <tr v-for="food, i in foods" :key="food.id">
+                        <td>{{food.id}}</td>
+                        <td>{{food.titlle}}</td>
+                        <td>{{food.created_at}}</td>
+                        <td>{{food.updated_at}}</td>
+                       
+                        <td>
 
+                        </td>
+                    
+                    </tr>
                 </tbody>
             </table>
         </div> 
@@ -25,7 +35,7 @@
             getFoods(){
                 axios.get('http://127.0.0.1:8000/api/foods').then(
                     response =>(
-                        this.products = response.data
+                        this.foods = response.data.data
                     )
                 );
             }
